872c754165afd0114152e54794ca2d7cd504f70b
ethankern <43477546+ethankern@users.noreply.github.com>
2018-09-21 12:15:06 -0600

    Created .xml request and set up .csv file

--- parse.py
+++ parse.py
@@ -1,2 +1,18 @@
 import csv
 import requests
+
+# Grab xml data using requests.
+URL = "http://syndication.enterprise.websiteidx.com/feeds/BoojCodeTest.xml"
+response = requests.get(URL)
+
+# Open xml file and write requests content to it.
+with open('BoojCodeTest.xml', 'wb) as file:
+          file.write(response.content)
+
+# Open a csv file and create csv writer object.
+Test_data = open('TestData.csv', 'w')
+csvwriter = csv.writer(Test_data)
+          
+
+          
+Test_data(close)
