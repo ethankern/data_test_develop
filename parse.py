f4152fc68e0fe8efcf0d20b3b3333bd4a9264cd6
ethankern <43477546+ethankern@users.noreply.github.com>
2018-09-24 15:38:26 -0600

    Extracted repeated data searching into a function.
    Lost Appliances and Rooms fields, need to force the text search of their child elements.

--- parse.py
+++ parse.py
@@ -19,58 +19,61 @@ tree = ET.parse("BoojCodeTest.xml")
 root = tree.getroot()
 
 
-# Function takes a listing (ET.element object) and returns string
-# containing what we want to sort the listings by.
-# Need this as the key for sorting by date or by anything else.
+          # Function takes a listing (ET.Element object) and returns string
+          # containing what we want to sort the listings by.
+          # Need this as the key for sorting by date or by anything else.
 def return_text(listing):
                                         # Can change DateListed to whatever child tag
                                         # we want to sort by.
           return listing.find('.//DateListed').text
 
+          # Function takes a listing argument and returns a row
+          # (a list) of items to be written to a csv.
+def get_data(listing):
+          
+          row = []            # Row to be populated by each listing and written to csv.
+
+          for field in header:          # Iterate the data search for every desired field.
+                    
+                    ETfield = listing.find('.//' + str(field))        # Element object in the tree
+                                                                      # containing our field's data.
+                    
+                    try:                # Add the element's text to the row, up to 200 chars.
+                              
+                              row.append(ETfield.text[:200])
+                              
+                    except (TypeError, AttributeError):     # If the element has no data, we need
+                                                            # to search its children.
+                                                            # We raise errors for trying to index or
+                                                            # call a method from a NoneType object.
+                              try:
+                                        fieldlist = []      # Create a secondary list for this field.
+                                        for child in ETfield.getchildren():     # Put each child's data
+                                                  fieldlist.append(child.text)  # into this list.
+                                        row.append(fieldlist)
+                                        
+                              except:
+                                        row.append('None')
+                                        
+          return row
+                                                  
 
 # Write csv header row.
 header = ['MlsId','MlsName','DateListed','StreetAddress','Price','Bedrooms','Bathrooms','Appliances','Rooms','Description']
 csvwriter.writerow(header)
 
 
+# Main function:
                                         # Iterate search over each Listing element in the tree,
                                         # and sort by the tag's text (a string) defined in return_text().
 for listing in sorted(root.findall('Listing'), key=return_text):
                                                   # Only want descriptions containing the word 'and',
                                                   # and only listings posted in 2016.
           if ' and ' in listing.find('.//Description').text and '2016' in listing.find('.//DateListed').text:
-                    
-                    row = []                  # A row to be populated for each listing.
           
                                                   # Search each desired tag for child elements 
                                                   # and add their text to the row.
-                    MlsId = listing.find('.//MlsId').text
-                    row.append(MlsId)
-                    MlsName = listing.find('.//MlsName').text
-                    row.append(MlsName)
-                    DateListed = listing.find('.//DateListed').text
-                    row.append(DateListed)
-                    StreetAddress = listing.find('.//StreetAddress').text
-                    row.append(StreetAddress)
-                    Price = listing.find('.//Price').text
-                    row.append(Price)
-                    Bedrooms = listing.find('.//Bedrooms').text
-                    row.append(Bedrooms)
-                    Bathrooms = listing.find('.//Bathrooms').text
-                    row.append(Bathrooms)
-                                                  # Elements with additional children
-                    Appliances = []               # have each child listed.
-                    for appliance in listing.findall('.//Appliance'):
-                              Appliances.append(appliance.text)
-                    row.append(Appliances)
-          
-                    Rooms = []
-                    for room in listing.findall('.//Room'):
-                              Rooms.append(room.text)
-                    row.append(Rooms)
-          
-                    Description = listing.find('.//Description').text[0:200]
-                    row.append(Description)
+                    row = get_data(listing)
           
                     csvwriter.writerow(row)
 
